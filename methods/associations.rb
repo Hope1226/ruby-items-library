621812b686ecb53ada1a07608e1328e9f6afbf2a
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-02-09 00:00:45 +0500

    Add association class

---
+++ methods/associations.rb
@@ -0,0 +1,21 @@
+class Associations
+  attr_accessor :labels, :authors, :genres
+
+  def initialize
+    @labels = []
+    @authors = []
+    @genres = []
+  end
+
+  def show_labels
+    # lgic for showing lables goes here
+  end
+
+  def show_authors
+    # lgic for showing lables goes here
+  end
+
+  def show_gendres
+    # lgic for showing lables goes here
+  end
+end
