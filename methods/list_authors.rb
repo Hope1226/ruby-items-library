f556bd96d055b505be19d06eded6852393fcd610
Carlos E. Feliz <cfelizmedina@gmail.com>
2022-02-08 13:22:47 -0400

    Fix linters and update  validate_date method

--- methods/list_authors.rb
+++ methods/list_authors.rb
@@ -1,13 +1,12 @@
-#here we should call the author memory, something like require '../save_data/author'
+# here we should call the author memory, something like require '../save_data/author'
 
-class AuthorList 
-    
-    def initialize()
-        @authors = []
-    end
+class AuthorList
+  def initialize()
+    @authors = []
+  end
 
-    def list_author 
-        puts 'Your list of authors:  '
-        @authos.each_with_index { |item, index| puts "#{index}) Author ~ #{author.first_name} #{author.last_name}"}
-    end
+  def list_author
+    puts 'Your list of authors:  '
+    @authos.each_with_index { |_item, index| puts "#{index}) Author ~ #{author.first_name} #{author.last_name}" }
+  end
 end
