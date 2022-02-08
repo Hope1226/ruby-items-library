5ea8fc4ad47e4ae54603d01754b80c583da056e5
Carlos E. Feliz <cfelizmedina@gmail.com>
2022-02-08 13:14:03 -0400

    Implement add game and list authors classes

---
+++ methods/list_authors.rb
@@ -0,0 +1,13 @@
+#here we should call the author memory, something like require '../save_data/author'
+
+class AuthorList 
+    
+    def initialize()
+        @authors = []
+    end
+
+    def list_author 
+        puts 'Your list of authors:  '
+        @authos.each_with_index { |item, index| puts "#{index}) Author ~ #{author.first_name} #{author.last_name}"}
+    end
+end
