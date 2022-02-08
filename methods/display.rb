40656aa9b27b7af53f3377decd273c835147d2d9
Umidjon Ustabaev <umidjonustabaev@gmail.com>
2022-02-09 00:00:59 +0500

    Add display module

--- methods/display.rb
+++ methods/display.rb
@@ -17,11 +17,16 @@ module Display
     author_first_name = gets.chomp
     print 'Last Name: '
     author_last_name = gets.chomp
-    print "Please enter the lebl (e.g. 'Gift', 'New'): "
-    label = gets.chomp
+    puts "Please enter the label (e.g. 'Gift', 'New'): "
+    print 'Title: '
+    title = gets.chomp
+    print 'Color: '
+    color = gets.chomp
     print "Please enter genre (e.g 'Comedy', 'Thriller'): "
     genre = gets.chomp
-    [author_first_name, author_last_name, label, genre]
+    print 'Please enter publish date in a format [ YYYY-MM-DD ]: '
+    publish_date = gets.chomp
+    [author_first_name, author_last_name, title, color, genre, publish_date]
   end
 
   def dis_book_data
@@ -29,6 +34,7 @@ module Display
     publisher = gets.chomp
     print 'Please enter the cover state [good, medium, bad]: '
     cover_state = gets.chomp
+    puts 'Book has been added successfully'
     [publisher, cover_state]
   end
 
@@ -37,11 +43,13 @@ module Display
     last_played_at = gets.chomp
     print 'Multiplayer? [ Yes : NO ]: '
     multiplayer = gets.chomp.downcase
+    puts 'Game has been added successfully'
     [last_played_at, multiplayer]
   end
 
   def dis_music_data
     print 'On spotify? [ Yes : No ]: '
+    puts 'Music album has been added successfully'
     gets.chomp.downcase.eql?('yes')
   end
 end
