2f3ef5641e6b49d20a0e504bac7447def42dd388
Carlos E. Feliz <cfelizmedina@gmail.com>
2022-02-07 22:56:35 -0400

    Create class author with all properties and methods

---
+++ author.rb
@@ -0,0 +1,17 @@
+require './item'
+
+class Author < Item
+    attr_accessor :first_name, :last_name, :items
+
+    def initialize (first_name, last_name)
+        @id = Random.rand(1..500)
+        @first_name = first_name
+        @last_name = last_name
+        @items = []
+    end
+
+    def add_item(item)
+        @items.push(self)
+        item.author = self
+    end
+end
