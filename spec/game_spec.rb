8087a325c2e60e41a988170405bdd8e01c0a296d
Carlos E. Feliz <cfelizmedina@gmail.com>
2022-02-10 10:45:53 -0400

    Fix linters

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,29 +1,29 @@
 require './game'
 
-describe Game do 
-    before :each do
-        @game = [Game.new('2021-02-23', true, '2010-01-30'), Game.new('2021-02-23', true, '2017-01-30')]
-    end
+describe Game do
+  before :each do
+    @game = [Game.new('2021-02-23', true, '2010-01-30'), Game.new('2021-02-23', true, '2017-01-30')]
+  end
 
-    it 'check if game is kind of Item' do
-        @game.each do |game|
-          expect(game).to be_kind_of Item
-        end
-      end
-    
-      it 'is an instance of the class?' do
-        @game.each do |game|
-          expect(game).to be_instance_of Game
-        end
-      end
-    
-      it 'check if the game can be archived' do
-        can_be_archived = @game[0].send(:can_be_archived?)
-        expect(can_be_archived).to be true
-      end
-    
-      it 'check if the game can not be archived' do
-        can_be_archived = @game[1].send(:can_be_archived?)
-        expect(can_be_archived).to be false
-      end
+  it 'check if game is kind of Item' do
+    @game.each do |game|
+      expect(game).to be_kind_of Item
+    end
+  end
+
+  it 'is an instance of the class?' do
+    @game.each do |game|
+      expect(game).to be_instance_of Game
+    end
+  end
+
+  it 'check if the game can be archived' do
+    can_be_archived = @game[0].send(:can_be_archived?)
+    expect(can_be_archived).to be true
+  end
+
+  it 'check if the game can not be archived' do
+    can_be_archived = @game[1].send(:can_be_archived?)
+    expect(can_be_archived).to be false
+  end
 end
